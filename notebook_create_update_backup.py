24bd79094bbce5b3425c6d8ea79d929b612d9db3
Andreas Heuser Nordgaard <131433344+aols0228@users.noreply.github.com>
2025-06-03 14:19:21 +0200

    backups fra notebook-deploy

---
+++ notebook_create_update_backup.py
@@ -0,0 +1,48 @@
+# Forfatter: jsju0002
+# Link til kilde:
+# Beskrivelse:
+## Pythonscript, der regexer create schema statements ud og replacer dem med create statements der passer til de separate miljøer.
+## Output er en .sql fil der kan oprette skemaet på det rigtige miljø
+# Changelog:
+# ---------------------------------------------------------------
+# Version | Dato       | Forfatter | Beskrivelse
+# 1.0.0     04-02-2025   jsju0002    Frigivelse af kode
+
+import os.path
+import re
+import sys
+
+def check_format_and_prepare_notebook(folder, domain, notebook_filename):
+    """ Takes a split notebook path and an environment letter.
+    Prepares the notebook to be executed against gold_dataenheden schema in the environment
+    """
+    with open(os.path.join(os.path.split(os.path.split(os.path.dirname(__file__))[0])[0],folder, domain, notebook_filename)) as notebook:
+        notebook_str = notebook.read()
+
+    # Search for valid create schema statements
+    if (matches := list(re.finditer(r"CREATE *SCHEMA[ |.*\n ]*IF *NOT *EXISTS *dap_._gold_dataenheden_sandbox\.(.*)\n*;",notebook_str))):
+        if len(matches) > 1:
+            raise Exception('Found multiple CREATE SCHEMA IF NOT EXISTS statements.')
+        else:
+            match = matches[0]
+    else:
+        raise Exception('Did not find a CREATE SCHEMA IF NOT EXISTS statement.')
+        
+    drop_create = f"DROP SCHEMA IF EXISTS dap_d_gold_dataenheden.{match.group(1)} CASCADE;\nCREATE SCHEMA IF NOT EXISTS dap_d_gold_dataenheden.{match.group(1)};"
+    # Add drop/create to notebook in correct place 
+    notebook_str = notebook_str[0:match.span()[0]] + drop_create + notebook_str[match.span()[1]:]
+    # Remove comments
+    # notebook_str = re.sub(r"\/\*[\s\S]*?\*\/", "", notebook_str)
+    
+    for env in ['d','t','p']:
+        with open(f'{env}_notebook.sql', 'w') as temp_notebook:
+            # Remove sandbox suffix
+            temp_str = re.sub(r"dap_[dtp]_gold_dataenheden_sandbox", f"dap_{env}_gold_dataenheden", notebook_str)
+            # Sub env for the current env
+            temp_notebook.write(re.sub(r"dap_[dtp]_", f"dap_{env}_", temp_str))
+
+if __name__ == '__main__':
+    #Split input path
+    temp = sys.argv[1].split('/')
+    check_format_and_prepare_notebook(temp[0], temp[1], temp[2])
+
---
+++ notebook_delete_backup.py
@@ -0,0 +1,40 @@
+# Forfatter: jsju0002
+# Link til kilde:
+# Beskrivelse:
+## Pythonscript, der regexer create schema statements ud og replacer dem med create statements der passer til de separate miljøer.
+## Output er en .sql fil der dropper skemaet.
+# Changelog:
+# ---------------------------------------------------------------
+# Version | Dato       | Forfatter | Beskrivelse
+# 1.0.0     04-02-2025   jsju0002    Frigivelse af kode
+
+import os.path
+import re
+import sys
+
+def drop_schema(folder, domain, notebook_filename):
+    """ Takes a split notebook path and an environment letter.
+    Prepares the notebook to be executed against gold_dataenheden schema in the environment
+    """
+    with open(os.path.join(os.path.split(os.path.split(os.path.dirname(__file__))[0])[0],folder, domain, notebook_filename)) as notebook:
+        notebook_str = notebook.read()
+
+    # Search for valid create schema statments
+    if (matches := list(re.finditer(r"CREATE *SCHEMA[ |.*\n ]*IF *NOT *EXISTS *dap_._gold_dataenheden_sandbox\.(.*)\n*;",notebook_str))):
+        if len(matches) > 1:
+            raise Exception('Found multiple CREATE SCHEMA IF NOT EXISTS statements.')
+        else:
+            match = matches[0]
+    else:
+        raise Exception('Did not find a CREATE SCHEMA IF NOT EXISTS statement.')
+
+    for env in ['d', 't', 'p']:
+        drop = notebook_str[0:match.span()[0]]+ f"DROP SCHEMA IF EXISTS dap_{env}_gold_dataenheden.{match.group(1)} CASCADE;"
+
+        with open(f'{env}_notebook.sql', 'w') as temp_notebook:
+            temp_notebook.write(drop)
+
+if __name__ == '__main__':
+    #Split input path
+    temp = sys.argv[1].split('/')
+    drop_schema(temp[0], temp[1], temp[2])
